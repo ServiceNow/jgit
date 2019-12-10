8516e8cb43c72ab2b39b515d328ad9221721aa96
krishnachaitanya.akkineni <krishnachaitanya.akkineni@servicenow.com>
2019-12-10 10:43:16 -0600

    STRY50557163 : [Delta-Loading]Find the delta of changes between currently loaded application files and new changes applied while Import from SC
    [Delta-Loading]Find the delta of changes between currently loaded application files and new changes applied while Import from SC
    
    This is a customization in the JGIT to filter out the noise and reduce the source control app install on the service now instance.
    
    The customization ONLY includes changes to DiffCommand and DiffFormatter and we exposed a new method and the purpose is to not break existing JGIT in any way.

--- org.eclipse.jgit/src/org/eclipse/jgit/diff/DiffFormatter.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/diff/DiffFormatter.java
@@ -62,7 +62,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Pattern;
 
 import org.eclipse.jgit.diff.DiffAlgorithm.SupportedAlgorithm;
