4e8ae79c38d6bf27fd8ec9f522b85ebf9df26d0d
Krishna Chaitanya Akkineni <krishnachaitanyaakkineni@gmail.com>
2019-12-12 10:10:27 -0600

    STRY50557163 : [Delta-Loading]Find the delta of changes  between currently loaded application files and new changes applied while  Import from SC (#4)
    [Delta-Loading]Find the delta of changes between currently loaded application files and new changes applied while Import from SC
    
    This is a customization in the JGIT to filter out the noise and reduce the source control app install on the service now instance.
    
    The customization ONLY includes changes to DiffCommand and DiffFormatter and we exposed a new method and the purpose is to not break existing JGIT in any way.

--- org.eclipse.jgit/src/org/eclipse/jgit/diff/DiffFormatter.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/diff/DiffFormatter.java
@@ -60,7 +60,9 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import org.eclipse.jgit.diff.DiffAlgorithm.SupportedAlgorithm;
 import org.eclipse.jgit.diff.DiffEntry.ChangeType;
@@ -144,6 +146,10 @@ public class DiffFormatter implements AutoCloseable {
 	private ProgressMonitor progressMonitor;
 
 	private ContentSource.Pair source;
+
+	private static final String EMPTY_STRING = "";
+
+
 
 	/**
 	 * Create a new formatter with a default level of context.
@@ -650,6 +656,38 @@ public class DiffFormatter implements AutoCloseable {
 		for (DiffEntry ent : entries)
 			format(ent);
 	}
+
+	/**
+	 * Format the diff entries by filtering out the noise from the given delta filter pattern
+	 * The filter acts only for files that have MODIFY change Type.
+	 * If there are no changes detected, we will remove the diff entry.
+	 * @param entries
+	 * @param deltaFilterPattern
+	 * @throws IOException
+	 */
+
+	public void format(List<? extends DiffEntry> entries, Pattern deltaFilterPattern) throws IOException {
+		if (deltaFilterPattern == null) {
+			format(entries);
+			return;
+		}
+		Iterator<? extends DiffEntry> diIterator = entries.iterator();
+		while (diIterator.hasNext()) {
+			DiffEntry diffEntry = diIterator.next();
+			if (MODIFY.equals(diffEntry.changeType)) {
+				FormatResult res = createFormatResult(diffEntry);
+				String aContent = new String(res.a.content);
+				String bContent = new String(res.b.content);
+				aContent = deltaFilterPattern.matcher(aContent).replaceAll(EMPTY_STRING);
+				bContent = deltaFilterPattern.matcher(bContent).replaceAll(EMPTY_STRING);
+				if (!aContent.equals(bContent))
+					format(res.header, res.a, res.b);
+				else
+					diIterator.remove();
+			} else format(diffEntry);
+		}
+	}
+
 
 	/**
 	 * Format a patch script for one file entry.
