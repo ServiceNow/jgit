5d1aeebcc6d7d8d797e02f8284ee02ea1fdfb052
Krishna Chaitanya Akkineni <krishnachaitanyaakkineni@gmail.com>
2019-12-12 10:10:27 -0600

    STRY50557163 : [Delta-Loading]Find the delta of changes  between currently loaded application files and new changes applied while  Import from SC (#4)
    [Delta-Loading]Find the delta of changes between currently loaded application files and new changes applied while Import from SC
    
    This is a customization in the JGIT to filter out the noise and reduce the source control app install on the service now instance.
    
    The customization ONLY includes changes to DiffCommand and DiffFormatter and we exposed a new method and the purpose is to not break existing JGIT in any way.

--- org.eclipse.jgit/src/org/eclipse/jgit/api/DiffCommand.java
+++ org.eclipse.jgit/src/org/eclipse/jgit/api/DiffCommand.java
@@ -48,6 +48,8 @@ import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.JGitInternalException;
@@ -94,6 +96,8 @@ public class DiffCommand extends GitCommand<List<DiffEntry>> {
 
 	private ProgressMonitor monitor = NullProgressMonitor.INSTANCE;
 
+	private Pattern deltaFilterPattern = null;
+
 	/**
 	 * Constructor for DiffCommand
 	 *
@@ -117,6 +121,9 @@ public class DiffCommand extends GitCommand<List<DiffEntry>> {
 	 * collected by the setter methods (e.g. {@link #setCached(boolean)} of this
 	 * class. Each instance of this class should only be used for one invocation
 	 * of the command. Don't call this method twice on an instance.
+	 *
+	 * Team-Devatscale SC customization to filter some files based on a regex pattern.
+	 * This filter will not be applied if showNameAndStatusOnly is set to true
 	 */
 	@Override
 	public List<DiffEntry> call() throws GitAPIException {
@@ -154,7 +161,9 @@ public class DiffCommand extends GitCommand<List<DiffEntry>> {
 					diffFmt.setNewPrefix(destinationPrefix);
 				if (sourcePrefix != null)
 					diffFmt.setOldPrefix(sourcePrefix);
-				diffFmt.format(result);
+
+				diffFmt.format(result, this.getDeltaFilterPattern());
+
 				diffFmt.flush();
 				return result;
 			}
@@ -285,6 +294,22 @@ public class DiffCommand extends GitCommand<List<DiffEntry>> {
 			monitor = NullProgressMonitor.INSTANCE;
 		}
 		this.monitor = monitor;
+		return this;
+	}
+
+	public Pattern getDeltaFilterPattern() {
+		return deltaFilterPattern;
+	}
+
+	/**
+	 * Set the given delta filter regex pattern. Used only for Source Control.
+	 *
+	 * @param deltaFilterPattern
+	 *            the filter pattern
+	 * @return this instance
+	 */
+	public DiffCommand setDeltaFilterPattern(Pattern deltaFilterPattern) {
+		this.deltaFilterPattern = deltaFilterPattern;
 		return this;
 	}
 }
